de9a5a01161c150cc54a286450b3e699e2795b46
Seung Won <ksw8736@naver.com>
2025-04-02 14:19:42 +0900

    5주차 수업 자바스크립트 활용

---
+++ js/js_popup.js
@@ -0,0 +1,4 @@
+function pop_up() {
+    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    }
+    // ../ 상대경로 js 폴더 안에 popup이라는 폴더가 없음 (상위 폴더) - js라는 파일에 나가서 popup을 찾아라 
