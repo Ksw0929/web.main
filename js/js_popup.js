b475068279e60ae7084e545b1b9c00368dd0f7aa
Seung Won <ksw8736@naver.com>
2025-04-09 13:56:15 +0900

    6주차 수업완료

--- js/js_popup.js
+++ js/js_popup.js
@@ -2,3 +2,31 @@ function pop_up() {
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
     // ../ 상대경로 js 폴더 안에 popup이라는 폴더가 없음 (상위 폴더) - js라는 파일에 나가서 popup을 찾아라 
+    function show_clock(){
+        let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
+        let divClock = document.getElementById('divClock');
+        let msg = "현재 시간 : ";
+        if(currentDate.getHours()>12){ // 12시 보다 크면 오후 아니면 오전
+           msg += "오후";
+           msg += currentDate.getHours()-12+"시";
+          }
+        else {
+              msg += "오전";
+              msg += currentDate.getHours()+"시";
+             }
+        msg += currentDate.getMinutes()+"분";
+        msg += currentDate.getSeconds()+"초";
+        divClock.innerText = msg;
+        if (currentDate.getMinutes()>58) { //정각 1분전 빨강색 출력
+            divClock.style.color="red";
+           }
+        setTimeout(show_clock, 1000); //1초마다 갱신
+        }
+
+        function over(obj) {
+            obj.src="image/pocket logo.png";
+            }
+            function out(obj) {
+            obj.src="image/pocket logo2 .png";
+            }
+            
