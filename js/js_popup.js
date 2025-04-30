a7ad2a4fc461c36276477bed3e09b4436ef7060a
Seung Won <ksw8736@naver.com>
2025-04-30 13:54:37 +0900

    9주차 쿠키 저장소를 이용한 데이터 저장 수업 완료료

--- js/js_popup.js
+++ js/js_popup.js
@@ -18,18 +18,18 @@ function pop_up() {
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
     // ../ 상대경로 js 폴더 안에 popup이라는 폴더가 없음 (상위 폴더) - js라는 파일에 나가서 popup을 찾아라 
-    function show_clock(){
-        let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
-        let divClock = document.getElementById('divClock');
-        let msg = "현재 시간 : ";
-        if(currentDate.getHours()>12){ // 12시 보다 크면 오후 아니면 오전
-           msg += "오후";
-           msg += currentDate.getHours()-12+"시";
-          }
-        else {
-              msg += "오전";
-              msg += currentDate.getHours()+"시";
-             }
+function show_clock(){
+    let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
+    let divClock = document.getElementById('divClock');
+    let msg = "현재 시간 : ";
+    if(currentDate.getHours()>12){ // 12시 보다 크면 오후 아니면 오전
+        msg += "오후";
+        msg += currentDate.getHours()-12+"시";
+        }
+    else {
+        msg += "오전";
+        msg += currentDate.getHours()+"시";
+        }
         msg += currentDate.getMinutes()+"분";
         msg += currentDate.getSeconds()+"초";
         divClock.innerText = msg;
@@ -38,6 +38,42 @@ function pop_up() {
            }
         setTimeout(show_clock, 1000); //1초마다 갱신
         }
+
+function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + ";expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+        }
+
+function getCookie(name) {
+                var cookie = document.cookie;
+                console.log("쿠키를 요청합니다.");
+                if (cookie != "") {
+                var cookie_array = cookie.split("; ");
+                for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                 if (cookie_name[0] == "popupYN") {
+                 return cookie_name[1];
+                 }
+                 }
+                 }
+                 return ;
+                }
+function closePopup() {
+         if (document.getElementById('check_popup').value) {
+            setCookie("popupYN", "N", 1);
+            console.log("쿠키를 설정합니다.");
+            self.close();
+             }
+         }                
+                
+function pop_up() {
+            var cookieCheck = getCookie("popupYN");
+            if (cookieCheck != "N"){
+            window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+            }
+            }
+            
 
         const over = (obj) => {
             obj.src="image/pocket logo.png";
